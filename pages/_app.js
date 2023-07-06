ccc119ff3f10c412ad0eb0d85b3801a360c473f0
kristopherjafeth <kristopherjafeth@gmail.com>
2023-07-06 16:44:11 -0400

    fix main

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,16 @@ import '@/components/Header/Header.css'
 import '@/components/Home/Home.css'
 import '@/components/Home/About.css';
 import '@/components/Home/Skills/Skills.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
  
   return (
     <div>
+      <Head>
+           <title>Kristopher Jafeth | Full Stack Web Developer ✅</title>
+           <meta name="description" content="I am creative web developer, Im very passionate and dedicated to my work, Welcome to my site!" />
+         </Head>
       <Header />
       <Component {...pageProps} />
     </div>
