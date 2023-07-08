3bbeff9459490f4305b029e60441c7e1c7c88a4b
kristopherjafeth <kristopherjafeth@gmail.com>
2023-07-08 00:58:33 -0400

    add services

--- components/Home/Skills.js
+++ components/Home/Skills.js
@@ -5,7 +5,7 @@ import Backend from './Skills/Backend'
 
 function Skills() {
   return (
-    <section className="about section" id="about">
+    <section className="about section" id="skills">
     <h1 className="section__title">Ski<span className="span-green">ll</span>s</h1>
     <span className="section__subtitle">My Stack</span>
         <div className='skills__container container grid'>
